fc215b2bdf283dd71942c02cb38b61b2c441315b
Infinidrix <biruksolomon11@gmail.com>
2021-02-18 09:22:12 +0300

    finish 2 template literals

--- Finished/asset/js/01(TemplateLiteral).js
+++ Finished/asset/js/01(TemplateLiteral).js
@@ -19,9 +19,9 @@ function templateStringConcatenation() {
 
     //1. Create a variable called fullIntroTemplate and Store the same string displayed above with template literal
 
-
+    const fullIntryTempate = `My Name is ${student.name} and i am ${student.age} Years Old`;
     //3. Display it [change it to fullIntroTemplate]
-    templateConcatenationResult.innerHTML = fullIntro;
+    templateConcatenationResult.innerHTML = fullIntryTempate;
 
 }
 
@@ -32,7 +32,7 @@ function displayTemplateExpression() {
     const result = "The Sum of the Numbers is  : " + sum(1, 2)
 
     //1. Create a variable called result1 and Store the same string displayed above with template literal
-
+    const result1 = `The Sum of the Numbers is ${sum(1, 2)}`;
 
     //3. Display it [change it to result1]
     templateExpression.innerHTML = result;
